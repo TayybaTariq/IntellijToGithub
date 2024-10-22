58a329187fa7d3e6afd0c01f9f164af4f29bff01
TayyabaTariqstu <2022se1@student.uet.edu.pk>
2024-10-22 23:16:20 +0500

    First Commit

---
+++ src/IntellijToGithub.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class IntellijToGithub {
+
+    public static void main(String[] args) {
+
+        System.out.println("Here I am going to push this code on remote repo(Github)");
+
+    }
+}
